77ff855cc507d06ed3fdc0f81cceebcb95fd3fea
Gun Woo Kim <closhuh@gmail.com>
2023-11-22 09:21:47 -0600

    initial file

---
+++ main.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(require csc151)
+(require 2htdp/image)
+(require rackunit)
+
+#| 𝐇 𝐄 𝐀 𝐃 𝐄 𝐑 _____________________________
+
+| CSC-151 Fall 2023
+| Mini Project 8
+| Authors: GunWoo Kim, Leonardo Nunes, Nicole Gonzalvez, Slok Rajbhandari
+| Date: 2023-11-22
+| Acknowledgements:
+|   - https://docs.racket-lang.org/guide/modules.html (to learn about modules)
+|   -
+|   - 
+|
+|#
