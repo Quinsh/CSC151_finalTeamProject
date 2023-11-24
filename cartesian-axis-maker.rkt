1f6895fecf85a184f7541243dfc1ade801a25e72
Nicole Moreno <nickymoreno1110@gmail.com>
2023-11-24 16:08:04 -0600

    documented code

--- cartesian-axis-maker.rkt
+++ cartesian-axis-maker.rkt
@@ -2,6 +2,12 @@
 (require 2htdp/image)
 (require csc151)
 
+;;; (cartesian-axis-maker width height) -> image?
+;;;   width: positive-integer? (equal or greater than 10)
+;;;   height: positive-integer? (equal or greater than 10)
+;;; Generates a width-height image containing the upper part of a
+;;; Cartesian Plane.
+
 (define cartesian-axis-maker
   (lambda (width height)
     (let* ([thickness (get-thickness width height)]
@@ -13,26 +19,10 @@
                                        (rotate 180 (axis 5 x-unit thickness))))
                (rectangle width height "outline" "black")))))
 
-(define stick
-  (lambda (length thickness)
-    (rectangle length thickness "solid" "black")))
-
-(define axis
-  (lambda (number unit-length thickness)
-    (axis/helper! 1 (* (- number 1) 2) unit-length thickness)))
-
-(define axis/helper!
-  (lambda (pos len unit-length thickness)
-    (if (equal? pos len)
-        (get-stick pos unit-length thickness)
-        (beside (get-stick pos unit-length thickness)
-                (axis/helper! (+ pos 1) len unit-length thickness)))))
-
-(define get-stick
-  (lambda (number unit-length thickness)
-    (if (zero? (remainder number 2))
-        (stick unit-length thickness)
-        (stick thickness (* 10 thickness)))))
+;;; (get-thickness width height) -> positive-real?
+;;;   width: positive-integer? (equal or greater than 10)
+;;;   height: positive-integer? (equal or greater than 10)
+;;; Sets the thickness of the axis depending on the width and height.
 
 (define get-thickness
   (lambda (width height)
@@ -45,9 +35,55 @@
         [(and (<= 10 side) (< side 300))
          0.5]))))
 
+;;; (axis number unit-length thickness) -> image?
+;;;   number: positive-integer? (numbers in the axis)
+;;;   unit-length: positive-real? (distance between two vertical little sticks)
+;;;   thickness: positive-real?
+;;; Creates axis with "number" number of points.
+
+(define axis
+  (lambda (number unit-length thickness)
+    (axis/helper 1 (* (- number 1) 2) unit-length thickness)))
+
+;;; (axis/helper pos len unit-length thickness) -> image?
+;;;   pos: positive-integer?
+;;;   len: positive-integer?
+;;;   unit-length: positive-real? (distance between two vertical little sticks)
+;;;   thickness: positive-real?
+;;; Creates axis with little sticks representing the units.
+
+(define axis/helper
+  (lambda (pos len unit-length thickness)
+    (if (equal? pos len)
+        (get-stick pos unit-length thickness)
+        (beside (get-stick pos unit-length thickness)
+                (axis/helper (+ pos 1) len unit-length thickness)))))
+
+;;; (get-stick number unit-length thickness) -> image?
+;;;   number: positive-integer? (numbers in the axis)
+;;;   unit-length: positive-real? (distance between two vertical little sticks)
+;;;   thickness: positive-real?
+;;; Returns the type of stick, if the number is even it gives
+;;; a horizontal stick, when it is odd it gives a vertical stick.
+
+(define get-stick
+  (lambda (number unit-length thickness)
+    (if (zero? (remainder number 2))
+        (stick unit-length thickness)
+        (stick thickness (* 10 thickness)))))
+
+;;; (stick length thickness) -> image?
+;;;   length: positive-real?
+;;;   thickness: positive-real?
+;;; Generates a stick with the given length and thickness.
+
+(define stick
+  (lambda (length thickness)
+    (rectangle length thickness "solid" "black")))
+
+
 (cartesian-axis-maker 800 1000)
 ;(cartesian-axis-maker 800 800)
 ;(cartesian-axis-maker 800 50)
 (cartesian-axis-maker 25 800)
 (cartesian-axis-maker 800 25)
-       
